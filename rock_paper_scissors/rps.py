29d9ae1caf984a878ba328ef1215783d0b2648c1
Connor <connorpheraty@gmail.com>
2019-10-10 16:26:23 -0500

    rock_paper_scissors complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,40 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def multiplier(cache):
+  base_cache = [['rock'], ['paper'], ['scissors']]
+  emt_cache = []
+  for i in cache:
+    for j in range(len(base_cache)):
+      n = i + base_cache[j]
+      emt_cache.append(n)
+      
+  return emt_cache
+
+def rock_paper_scissors(n, cache=None):
+  if n == 0:
+    if not cache:
+      cache = [[]]
+    return cache
+  
+  if n == 1:
+    if not cache:
+      cache = [['rock'], ['paper'], ['scissors']]
+      return cache
+    cache = multiplier(cache)
+    cache = rock_paper_scissors(n-1, cache)
+    return cache
+  
+  
+  else:
+    if not cache:
+      cache = [['rock'], ['paper'], ['scissors']]
+      n -= 1
+    cache = multiplier(cache)
+    
+    cache = rock_paper_scissors(n-1, cache)
+    
+    return cache
 
 
 if __name__ == "__main__":
